be1deefe42fca22d718e336e7307c4da60ad126c
edwin <edwinho1501@gmail.com>
2024-05-11 07:55:37 +0800

    add test page and image generator script

---
+++ image_generator/image_generator.py
@@ -0,0 +1,43 @@
+from PIL import Image
+
+
+color_map = {
+    "red": (255, 0, 0),
+    "green": (0, 200, 0),
+    "blue": (0, 0, 255),
+    "yellow": (240, 240, 30)
+}
+
+
+for image_name in color_map.keys():
+    # Import an image from directory: 
+    input_image = Image.open("resource\\word\\" + image_name + ".png") 
+    
+    for color in color_map.keys():
+        image1 = Image.new("RGBA", input_image.size)
+        image1.paste(input_image) 
+        
+        # Extracting pixel map: 
+        pixel_map = image1.load() 
+        
+        # Extracting the width and height  
+        # of the image: 
+        width, height = image1.size 
+
+        for i in range(width): 
+            for j in range(height): 
+                
+                # getting the RGB pixel value. 
+                r, g, b, p = image1.getpixel((i, j)) 
+                ave = (r + g + b)//3
+                    
+                # setting the pixel value.
+                if (ave <= 230): 
+                    pixel_map[i, j] = color_map[color]
+    
+        # Saving the final output 
+        image1.save("output\\word\\" + image_name + "_" + color + ".png", format="png") 
+    
+        # image1.show()
+        
+    
